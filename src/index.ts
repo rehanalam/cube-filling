a8caf4c55aab08a844a4a088d7576177ed968158
Muhammad Rehan Alam <rehan@beebolt.com>
2025-03-16 11:11:06 +0300

    chore: refactored code

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,10 @@
-const fs = require('fs');  // CommonJS require for fs
+const fs = require('fs');
 const path = require('path');
 
 interface ICube {
     quantity: number;
     size: number;
 }
-
-const inputMockData: (number[])[] = [
-    [10, 10, 10, 2000],
-    [10, 10, 10, 900],
-    [4, 4, 8, 10, 10, 1],
-    [5, 5, 5, 61, 7, 1],
-    [5, 5, 6, 61, 4, 1],
-    [1000, 1000, 1000, 0, 0, 0, 46501, 0, 2791, 631, 127, 19, 1],
-    [1, 1, 9, 9, 1]
-];
 
 function computeCubeData(cubes: number[]): ICube[] {
     return cubes.map((cube, index) => {
@@ -67,7 +57,6 @@ function readDataFromFile(filename: string): number[][] {
     const parsedData: number[][] = lines.map((line: string) => {
         return line.split(' ').map(Number);
     });
-    console.log(parsedData);    
     return parsedData;
 }
 
@@ -75,4 +64,3 @@ function readDataFromFile(filename: string): number[][] {
 const data = readDataFromFile('problem.txt');
 const results = computeInput(data);
 console.log(results);
-// console.log(computeInput(inputMockData));
